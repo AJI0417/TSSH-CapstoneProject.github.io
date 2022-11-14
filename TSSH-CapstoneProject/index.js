06227f4486daa2ce854d606dd89109b4918efb17
阿極 <76050035+AJI0417@users.noreply.github.com>
2022-11-14 18:57:39 +0800

    Version 1.0

--- TSSH-CapstoneProject/index.js
+++ TSSH-CapstoneProject/index.js
@@ -1,10 +1,13 @@
 /*時間顯示*/
 function showTime() {
-  var date = new Date();
-  var h = date.getHours(); // 0 - 23
-  var m = date.getMinutes(); // 0 - 59
-  var s = date.getSeconds(); // 0 - 59
-  var session = "AM";
+  let date = new Date();
+  let h = date.getHours(); // 0 - 23
+  let m = date.getMinutes(); // 0 - 59
+  let s = date.getSeconds(); // 0 - 59
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let session = "AM";
 
   if (h == 0) {
     h = 12;
@@ -12,17 +15,26 @@ function showTime() {
 
   if (h > 12) {
     h = h - 12;
-    session = "PM(GMT+8)";
+    session = " PM";
   }
 
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
 
-  var time = h + ":" + m + ":" + s + " " + session;
+  let time =
+    year + "-" + month + "-" + day + " " + h + ":" + m + ":" + s + session;
   document.getElementById("MyClockDisplay").innerText = time;
-  document.getElementById("MyClockDisplay").textContent = time;
 
-  setTimeout(showTime, 1000);
+  setInterval(showTime, 1000);
 }
 showTime();
+
+/*替換AOS Animation*/
+let MobileScreen = screen.width;
+let itemDataset = document.querySelectorAll(".item");
+itemDataset.forEach((index) => {
+  if (MobileScreen <= 760) {
+    index.dataset.aos = "fade-right";
+  }
+});
